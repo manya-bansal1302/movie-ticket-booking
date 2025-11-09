327f2d6f90b602a40221d8705da789a246474745
ManyaBansal <manyabansal1302@gmail.com>
2025-11-09 19:48:27 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -9,23 +9,19 @@ import { inngest, functions } from "./inngest/index.js";
 const app = express();
 const port = 3000;
 
-app.use(clerkMiddleware());
-
 await connectDB();
 
-// Middleware
+//Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
-//API Routes
-
-// Home route
 app.get("/", (req, res) => {
-  res.send("Welcome to the Home Page");
+  res.send("Welcome to the Movie Booking App API");
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
